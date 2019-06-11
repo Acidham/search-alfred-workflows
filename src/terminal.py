87d9d5e5d6d2d225cf0530adacc82fd52f316c7b
Acidham <acidham@gmail.com>
2019-06-11 15:24:16 +0200

    Fixed an issue with getting env

--- src/terminal.py
+++ src/terminal.py
@@ -7,7 +7,7 @@ from Alfred import Tools
 OS_TERMINAL = 'open -b com.apple.terminal'
 term = Tools.getEnv('terminal_path')
 terminal = term if term else OS_TERMINAL
-path = Tools.getEnv('item1')
+path = Tools.getEnv('plist_path')
 
 cmd = "%s %s" % (terminal, path)
 os.popen2(cmd)
